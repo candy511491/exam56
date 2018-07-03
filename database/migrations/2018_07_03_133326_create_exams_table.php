f8c91ac3fd1fccc643fefbc507a4030744b8796f
candy511491 <40749497+candy511491@users.noreply.github.com>
2018-07-03 14:38:08 +0800

    7_OK

--- database/migrations/2018_07_03_133326_create_exams_table.php
+++ database/migrations/2018_07_03_133326_create_exams_table.php
@@ -15,14 +15,21 @@ class CreateExamsTable extends Migration
     {
         Schema::create('exams', function (Blueprint $table) {
             $table->increments('id');
+            $table->text('content');
+            $table->unsignedInteger('exam_id');
+            $table->foreign('exam_id')->references('id')->on('exams');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedTinyInteger('score');
+
             $table->text('topic');
-$table->unsignedInteger('exam_id');
-$table->foreign('exam_id')->references('id')->on('exams')->onDelete('cascade');
-$table->text('opt1');
-$table->text('opt2');
-$table->text('opt3');
-$table->text('opt4');
-$table->unsignedTinyInteger('ans');
+            $table->unsignedInteger('exam_id');
+            $table->foreign('exam_id')->references('id')->on('exams')->onDelete('cascade');
+            $table->text('opt1');
+            $table->text('opt2');
+            $table->text('opt3');
+            $table->text('opt4');
+            $table->unsignedTinyInteger('ans');
 
             $table->string('title');
             $table->integer('user_id')->unsigned();
