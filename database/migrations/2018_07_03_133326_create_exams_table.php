4a4e5dcdc4fcc85f957b22d0d104cddc3a606332
candy511491 <40749497+candy511491@users.noreply.github.com>
2018-07-03 14:27:57 +0800

    7-2

--- database/migrations/2018_07_03_133326_create_exams_table.php
+++ database/migrations/2018_07_03_133326_create_exams_table.php
@@ -15,6 +15,19 @@ class CreateExamsTable extends Migration
     {
         Schema::create('exams', function (Blueprint $table) {
             $table->increments('id');
+            $table->text('topic');
+$table->unsignedInteger('exam_id');
+$table->foreign('exam_id')->references('id')->on('exams')->onDelete('cascade');
+$table->text('opt1');
+$table->text('opt2');
+$table->text('opt3');
+$table->text('opt4');
+$table->unsignedTinyInteger('ans');
+
+            $table->string('title');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->boolean('enable');
             $table->string('title');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
